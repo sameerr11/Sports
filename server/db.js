d2d39a7155d670c824eeb72febda077f2edfbbed
Ahmed Mustafa <122228024+RAhmedM@users.noreply.github.com>
2025-06-05 04:24:22 +0500

    Mongo URI update

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://sameersaif2002:KrgxJfAZ0t2PlcXz@sportsmanagement.mwplt.mongodb.net/sports?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/sports";
 
 
 const connectDB = async () => {
